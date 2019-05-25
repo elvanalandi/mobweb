c8ddb828eab146b11de1538f7fa6677c7873b8f0
SirTapir <37269469+SirTapir@users.noreply.github.com>
2019-05-25 16:31:04 +0700

    Offline capabilites & Folder reorganization
    Added a service worker sw.js
    Changed the location of images,css,and script.js into the src folder

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('first-app').then(function(cache) {
+      return cache.addAll([
+          '/index.php',
+          '/navbar.php',
+          '/highscore.php',
+          '/src/css/styles.css',
+          '/src/js/script.js'
+        // etc
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    // Try the cache
+    caches.match(event.request).then(function(response) {
+      // Fall back to network
+      return response || fetch(event.request);
+    })
+  );
+});
